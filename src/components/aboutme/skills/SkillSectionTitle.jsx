abdfcda0a5e02235818fff0776fd61cb2b1cd573
itshichabk <itshichabk@gmail.com>
2025-05-03 21:49:17 -0400

    Add SkillSectionTitle

---
+++ src/components/aboutme/skills/SkillSectionTitle.jsx
@@ -0,0 +1,5 @@
+export default function SkillSectionTitle({children}) {
+  return (
+    <h2 className="mt-6 mb-2 text-holo font-bold text-2xl">{children}</h2>
+  )
+}
--- src/components/aboutme/skills/SkillsRowSection.jsx
+++ src/components/aboutme/skills/SkillsRowSection.jsx
@@ -1,11 +1,12 @@
 import { skillsDB } from "@/src/utils/db";
 import TilesRow from "./TilesRow";
+import SkillSectionTitle from "./SkillSectionTitle";
 
 export default async function SkillsRowSection({title, collectionName}) {
     const langs = await skillsDB(collectionName);
     return (
     <>
-        <h2 className="mt-6 mb-2 text-holo font-bold text-2xl">{title}</h2>
+        <SkillSectionTitle>{title}</SkillSectionTitle>
         <TilesRow skills={langs}/>
     </>)
 }
